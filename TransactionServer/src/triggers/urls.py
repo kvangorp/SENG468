53916fa67cbbe4cc7dc50d4e8051c06ca2bbbae1
Katelyn Van Gorp <katelyn.vangorp@gmail.com>
2021-02-11 11:32:49 -0800

    Handle SET SELL TRIGGER and CANCEL SET SELL

--- TransactionServer/src/triggers/urls.py
+++ TransactionServer/src/triggers/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TriggerListView
+from .views import TriggerListView, TriggerDetailView
 
 urlpatterns = [
-    path('triggers/', TriggerListView.as_view())
+    path('triggers/', TriggerListView.as_view()),
+    path('triggers/<int:pk>/', TriggerDetailView.as_view())
 ]
--- TransactionServer/src/triggers/views.py
+++ TransactionServer/src/triggers/views.py
@@ -1,16 +1,22 @@
-from rest_framework import generics, status
+from django_filters.rest_framework import DjangoFilterBackend
+from django.http import Http404
+from rest_framework import generics, status, mixins
 from rest_framework.response import Response
 from .serializers import TriggerSerializer
 from .models import Trigger
 from stocks.models import Stock
+from accounts.models import Account
 
-class TriggerListView(generics.GenericAPIView):
+class TriggerListView(generics.GenericAPIView, mixins.ListModelMixin):
     serializer_class = TriggerSerializer
     queryset = Trigger.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['userId', 'stockSymbol']
 
-    # Handle set sell commands
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
     def post(self, request):
-
         # Make sure request can be serialized
         serializer = TriggerSerializer(data=request.data)
         if not serializer.is_valid():
@@ -26,7 +32,7 @@ class TriggerListView(generics.GenericAPIView):
             stockSymbol=stockSymbol
         ).first()
         
-        if stockAccount.shares < sharesToSell:
+        if stockAccount is None or stockAccount.shares < sharesToSell:
             return Response("You don't have enough stocks.", status=status.HTTP_412_PRECONDITION_FAILED)
 
         # Take specified number of stocks out of user's stock account
@@ -45,5 +51,54 @@ class TriggerListView(generics.GenericAPIView):
         # Add the new trigger to the database
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class TriggerDetailView(generics.GenericAPIView):
+    serializer_class = TriggerSerializer
+    queryset = Trigger.objects.all()
+
+    def get_object(self, pk):
+        try:
+            return Trigger.objects.get(pk=pk)
+        except Trigger.DoesNotExist:
+            raise Http404
+
+    def put(self, request, pk):
+        trigger = self.get_object(pk)
+
+        # Make sure request can be serialized
+        serializer = TriggerSerializer(trigger, data=request.data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        # Save updated trigger
+        serializer.save()
+        return Response(serializer.data)
+
+
+    def delete(self, request, pk):
+        trigger = self.get_object(pk)
+
+        # Remove reserved stock account
+        reservedAccount = Stock.objects.filter(
+            userId=trigger.userId,
+            stockSymbol=trigger.stockSymbol,
+            reserved=True
+        ).first()
+        reservedAccount.delete()
+
+        # Add stocks back into regular stock account
+        stockAccount = Stock.objects.filter(
+            userId=trigger.userId,
+            stockSymbol=trigger.stockSymbol,
+            reserved=False
+        ).first()
+        stockAccount.shares += trigger.shares
+        stockAccount.save()
+
+        # Delete trigger
+        trigger.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
     
