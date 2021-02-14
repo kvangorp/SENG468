bed0e46f135cfc44a71038ec4f48d665ff8d4c02
Tram <tram.ungtt@gmail.com>
2021-02-14 11:34:58 -0800

    Add COMMIT_SELL to generator

--- workload_generator/generator.py
+++ workload_generator/generator.py
@@ -44,8 +44,11 @@ def commandSwitch(command):
         print(command[0])
         cancel_buy(command[1])
     elif command[0] == 'SELL':
-         sell(command[1], command[2], command[3]) #userId, symbol, dollaramount
-	# elif command == 'COMMIT_SELL':
+        print(command[0])
+        sell(command[1], command[2], command[3]) #userId, symbol, dollaramount
+    elif command[0] == 'COMMIT_SELL':
+        print(command[0])
+        commit_sell(command[1])
 	# elif command == 'CANCEL_SELL':
 	# elif command == 'SET_BUY_AMOUNT':
 	# elif command == 'CANCEL_SET_BUY':
@@ -97,66 +100,70 @@ def buy(userid, stock, dollar_amount):
     else:
         user_command_log(userid, dollar_amount, 'BUY', stock)
 
-#TODO: add status field to buy commands: pending, commited, cancelled
-def commit_buy(userid):
+def commit_checker(userid, command):
     data = {
         'userId': userid,
-        'userCommand': 'BUY'
+        'userCommand': command
     }
     res = requests.get('http://localhost:8000/api/transactions/', params=data)
     print(res)
     list_of_transactions = res.json()
     print(list_of_transactions)
-    latest_buy = sorted(list_of_transactions, key=lambda k: k['timestamp'], reverse=True)
-    buy_time = ''
+    latest_command = sorted(list_of_transactions, key=lambda k: k['timestamp'], reverse=True)
+    command_time = ''
     if list_of_transactions:
-        buy_time = float(latest_buy[0]['timestamp'])
-        stock = latest_buy[0]['stockSymbol']
-        amount = latest_buy[0]['amount']
+        command_time = float(latest_command[0]['timestamp'])
+        stock = latest_command[0]['stockSymbol']
+        amount = latest_command[0]['amount']
     else:
         return
 
     data = {
         'userId': userid,
-        'userCommand': 'COMMIT_BUY'
+        'userCommand': 'COMMIT_' + command
     }
     res1 = requests.get('http://localhost:8000/api/transactions/', params=data)
     print(res1)
     list_of_transactions1 = res1.json()
     print(list_of_transactions1)
-    latest_commit_buy = sorted(list_of_transactions1, key=lambda k: k['timestamp'], reverse=True)
+    latest_commit_command = sorted(list_of_transactions1, key=lambda k: k['timestamp'], reverse=True)
     commit_time = ''
     if list_of_transactions1:
-        commit_time = float(latest_commit_buy[0]['timestamp'])
+        commit_time = float(latest_commit_command[0]['timestamp'])
 
     data = {
         'userId': userid,
-        'userCommand': 'CANCEL_BUY'
+        'userCommand': 'CANCEL_' + command
     }
     res2 = requests.get('http://localhost:8000/api/transactions/', params=data)
     print(res2)
     list_of_transactions2 = res2.json()
     print(list_of_transactions2)
-    latest_cancel_buy = sorted(list_of_transactions2, key=lambda k: k['timestamp'], reverse=True)
+    latest_cancel_command = sorted(list_of_transactions2, key=lambda k: k['timestamp'], reverse=True)
     cancel_time = ''
     if list_of_transactions2:
-        cancel_time = float(latest_cancel_buy[0]['timestamp'])
+        cancel_time = float(latest_cancel_command[0]['timestamp'])
     flag1 = False
     if not commit_time:
         flag1 = True
-    elif buy_time < commit_time:
+    elif command_time < commit_time:
         flag1 = True
     else:
         flag1 = False
     flag2 = False
     if not cancel_time:
         flag2 = True
-    elif buy_time < cancel_time:
+    elif command_time < cancel_time:
         flag2 = True
     else:
         flag2 = False
+    is_committable = flag1 and flag2 and ((time() - command_time) <= 60.0)
+    return is_committable, stock, amount
 
-    if flag1 and flag2 and ((time() - buy_time) <= 60.0):
+#TODO: add status field to buy commands: pending, commited, cancelled
+def commit_buy(userid):
+    is_committable, stock, amount = commit_checker(userid, 'BUY')
+    if is_committable:
         stock_quote = quote(userid, stock)
         shares = amount/stock_quote
         data = {
@@ -197,14 +204,27 @@ def sell(userid, stock, amount):
     stock_quote = quote(userid, stock)
     shares = float(amount)/stock_quote
     
-
     if account['shares'] < shares:
         print("yeah no")
     else:
+        print("you have enough shares to sell")
         user_command_log(userid, amount, 'SELL', stock)
 
-
-
+def commit_sell(userid):
+    is_committable, stock, amount = commit_checker(userid, 'SELL')
+    if is_committable:
+        stock_quote = quote(userid, stock)
+        shares = amount/stock_quote
+        data = {
+            'userId': userid,
+            'stockSymbol': stock,
+            'shares': shares,
+            'amount': amount
+        }
+        print(data)
+        res = requests.put(f'http://localhost:8000/api/stocks/sell/{userid}/{stock}/', json=data)
+        print(res.json)
+    user_command_log(userid, amount, 'COMMIT_SELL', stock)
 
 
 
