055a42ac006d4d1f428e364e019b8d3a628e5cb5
Tram <tram.ungtt@gmail.com>
2021-02-14 00:07:04 -0800

    Update stocks models, urls, and views

--- TransactionServer/src/stocks/models.py
+++ TransactionServer/src/stocks/models.py
@@ -4,4 +4,4 @@ class Stock(models.Model):
     userId = models.CharField(max_length=50)
     stockSymbol = models.CharField(max_length=50, null=False)
     shares = models.FloatField(null=False)
-    reserved = models.BooleanField(default=False)
+    #TODO: add constraint for userId and stockSymbol
--- TransactionServer/src/stocks/views.py
+++ TransactionServer/src/stocks/views.py
@@ -16,43 +16,49 @@ class StockListView(generics.GenericAPIView, mixins.ListModelMixin):
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
-    def post(self, request):
-        # Make sure request can be serialized
-        serializer = StockSerializer(data=request.data)
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class StockDetailView(generics.GenericAPIView, mixins.RetrieveModelMixin):
+    serializer_class = StockSerializer
+    queryset = Stock.objects.all()
 
-        # Check that the user has enough of the specified stock to sell
+    #TODO: add check for buy vs sell
+    def put(self, request, userId, stock):
+        # Get request data
         userId = request.data.get("userId")
         stockSymbol = request.data.get("stockSymbol")
-        sharesToSell = request.data.get("shares")
+        shares = float(request.data.get("shares"))
+        amount = float(request.data.get("amount"))
 
+        # Find user account
+        userAccount = Account.objects.filter(
+            userId=userId,
+        ).first()
+
+        if userAccount is None:
+            return Response("Account doesn't exist.", status=status.HTTP_412_PRECONDITION_FAILED)
+        if userAccount.balance < amount:
+            return Response("You don't have enough money.", status=status.HTTP_412_PRECONDITION_FAILED)
+
+        # Find stock account
         stockAccount = Stock.objects.filter(
             userId=userId,
             stockSymbol=stockSymbol
         ).first()
-        
-        if stockAccount is None or stockAccount.balance < sharesToSell:
-            return Response("You don't have enough stocks to sell.", status=status.HTTP_412_PRECONDITION_FAILED)
 
-        # Take specified number of stocks out of user's stock account
-        stockAccount.shares -= sharesToSell
+        if stockAccount is None:
+            stockAccount = Stock(
+                userId=userId,
+                stockSymbol=stockSymbol,
+                shares=0.0
+            )
+
+        # Decrement user balance amount
+        userAccount.balance -= amount
+        userAccount.save()
+
+        # Add stock shares
+        stockAccount.shares += shares
         stockAccount.save()
-        
-        # Put specified number of stocks aside in a reserved account
-        reservedStock = Stock(
-            userId=userId,
-            stockSymbol=stockSymbol,
-            shares=sharesToSell,
-            reserved=True
-        )
-        reservedStock.save()
 
-        # Add the new trigger to the database
-        serializer.save()
+        # Make sure request can be serialized
+        serializer = StockSerializer(stockAccount)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-class StockDetailView(generics.GenericAPIView, mixins.ListModelMixin):
-    serializer_class = StockSerializer
-    queryset = Stock.objects.all()
