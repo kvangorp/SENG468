bf377a9cd3141bd6d537967348455db3d9cc9845
Tram <tram.ungtt@gmail.com>
2021-02-13 18:42:21 -0800

    Update Accounts

--- TransactionServer/src/accounts/urls.py
+++ TransactionServer/src/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import AccountDetailView
+from .views import AccountDetailView, AccountListView
 
 urlpatterns = [
-    path('accounts/<int:pk>/', AccountDetailView.as_view())
+    path('accounts/<int:pk>/', AccountDetailView.as_view()),
+    path('accounts/', AccountListView.as_view())
 ]
