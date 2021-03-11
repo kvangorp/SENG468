f9c99e20a7a638d24bba326bf6d3dc483b8e6723
Connor <36247578+UmbraSang@users.noreply.github.com>
2021-03-10 19:18:22 -0800

    hash_key updates

--- transaction_server/src/commands/transactionsLogger.py
+++ transaction_server/src/commands/transactionsLogger.py
@@ -7,7 +7,7 @@ redis_instance = redis.StrictRedis(charset="utf-8", decode_responses=True, host=
 def log_quote_server_transaction(transactionNum, price, stockSymbol, username, quoteServerTime, cryptokey):
     transaction = {
         "type": "quoteServer",
-        "timestamp": int(time())*1000,
+        "timestamp": int(time()*1000),
         "server": "QS",
         "transactionNum": transactionNum, 
         "price": price,
@@ -16,31 +16,31 @@ def log_quote_server_transaction(transactionNum, price, stockSymbol, username, q
         "quoteServerTime": quoteServerTime,
         "cryptokey": cryptokey
     }
-    hash_key = str(transaction["timestamp"]) + os.environ['serverNum']
+    hash_key = str(transaction["timestamp"]*10) + os.environ['serverNum']
     redis_instance.hmset(hash_key, transaction)
 
 def log_account_transaction(transactionNum, action, username, funds):
     transaction = {
         "type": "accountTransaction",
-        "timestamp": int(time())*1000,
+        "timestamp": int(time()*1000),
         "server": 'TS',
         "transactionNum": transactionNum,
         "action": action,
         "username": username,
         "funds": funds
     }
-    hash_key = str(transaction["timestamp"]) + os.environ['serverNum']
+    hash_key = str(transaction["timestamp"]*10) + os.environ['serverNum']
     redis_instance.hmset(hash_key, transaction)
 
 def log_error_event(transactionNum, command, username, errorMessage):
     transaction = {
         "type": "errorEvent",
-        "timestamp": int(time())*1000,
+        "timestamp": int(time()*1000),
         "server":'TS',
         "transactionNum": transactionNum,
         "command": command,
         "username": username,
         "errorMessage": errorMessage
     }
-    hash_key = str(transaction["timestamp"]) + os.environ['serverNum']
+    hash_key = str(transaction["timestamp"]*10) + os.environ['serverNum']
     redis_instance.hmset(hash_key, transaction)
