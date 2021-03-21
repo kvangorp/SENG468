73937575a0a7a47f4a95970d5cf3bfb1b05c27a6
Tram <tram.ungtt@gmail.com>
2021-03-20 20:23:42 -0700

    Add gunicorn.conf and add more servers

---
+++ transaction_server/src/gunicorn.conf.py
@@ -0,0 +1,11 @@
+bind = '0.0.0.0:8000'
+backlog = 2048
+workers = 2
+worker_class = 'sync'
+worker_connections = 1200
+timeout = 30
+keepalive = 2
+spew = False
+errorlog = '-'
+loglevel = 'debug'
+accesslog = '-'
