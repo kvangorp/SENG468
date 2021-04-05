3c7fe3531fde788ae785a5b96f95f0c9e0d76292
Katelyn Van Gorp <katelyn.vangorp@gmail.com>
2021-04-05 14:32:14 -0700

    Containerize web server and fix server duplication

--- scripts/local_set_up.sh
+++ scripts/local_set_up.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker-compose -f docker-compose.local.yml build
-docker-compose -f docker-compose.local.yml up
+docker-compose -f docker-compose.local.yml up --scale server=10
 exec "$@"
--- scripts/vm_set_up.sh
+++ scripts/vm_set_up.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sudo docker-compose -f docker-compose.vm.yml build
-sudo docker-compose -f docker-compose.vm.yml up
+sudo docker-compose -f docker-compose.vm.yml up --scale server=10
 exec "$@"
